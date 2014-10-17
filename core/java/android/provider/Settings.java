ba486b0d8c460fd9ebcf0b603009d3cd57646052
Jubakuba <Jubakuba@gmail.com>
2014-10-17 00:39:40 -0600

    Framework: StatusBar Ticker (1/2)
    Change-Id: I6135297792c7b42172e08e14acd37b252cb00102
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2215,6 +2215,13 @@ public final class Settings {
         public static final String DOZE_BRIGHTNESS = "doze_brightness";
 
         /**
+         * Whether to enable ticker animation in statusbar
+         *
+         * @hide
+         */
+        public static final String STATUS_BAR_TICKER_ENABLED = "status_bar_ticker_enabled";
+
+        /**
          * Control whether the process CPU usage meter should be shown.
          *
          * @deprecated Use {@link Global#SHOW_PROCESSES} instead
