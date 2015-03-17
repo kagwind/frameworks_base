c7d677e608d2fab84bdb75606c7235586c8fc46f
Shaoshan Fan <a18273@motorola.com>
2015-03-17 13:19:53 -0500

    Fix logic to call userActivity with FLAG_KEEP_SCREEN_ON
    Call userActivity when release screen on lock only if keyguard is
    not showing
    
    Change-Id: I071bf2129ad53c0142e63a031a4ba90ef2a15b5e

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -7155,7 +7155,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
     @Override
     public void keepScreenOnStoppedLw() {
-        if (isKeyguardShowingAndNotOccluded()) {
+        if (!isKeyguardShowingAndNotOccluded()) {
             mPowerManager.userActivity(SystemClock.uptimeMillis(), false);
         }
     }
