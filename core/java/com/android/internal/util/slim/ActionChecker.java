2dd3d60ae9f82f734f17d7aaff2f982270bf4aac
GWX <kagwind@gmail.com>
2015-05-29 22:38:37 +0800

    Fix the logic of existence of main keys in navbar.
    Right now the only purpose of ActionChecker is to make sure that all
    main keys (Back/Home/Recents) in navbar exist. Consider these keys are
    unique, and their identities are completely based on their Click actions,
    this simplified logic should be enough to ensure their existence.
    
    In addition, it fixes a bug that, for example, users can remove Back key
    if its actions (e.g. Click & Longpress) are both "Back", and ends up with
    no Back key.

--- core/java/com/android/internal/util/slim/ActionChecker.java
+++ core/java/com/android/internal/util/slim/ActionChecker.java
@@ -24,43 +24,15 @@ import java.util.ArrayList;
 
 public class ActionChecker {
 
-    private static final ArrayList<String> mConfigs = new ArrayList<String>();
-
-    static {
-        mConfigs.add(Settings.System.NAVIGATION_BAR_CONFIG);
-    }
-
     public static boolean actionConfigContainsAction(ActionConfig config, String action) {
-        return action.equals(config.getClickAction())
-                || action.equals(config.getLongpressAction())
-                || action.equals(config.getDoubleTapAction());
+        return action.equals(config.getClickAction());
     }
 
-    public static boolean containsAction(Context context,
-            ActionConfig config, String action) {
-
-        if (!actionConfigContainsAction(config, action)) return true;
-
-        for (int i = 0; i < mConfigs.size(); i++) {
-            String configsString = Settings.System.getStringForUser(context.getContentResolver(),
-                    mConfigs.get(i), UserHandle.USER_CURRENT);
-
-            if (configsString.contains(ActionConstants.ACTION_BACK)) {
-                String input = configsString;
-                int index = input.indexOf(ActionConstants.ACTION_BACK);
-                int count = 0;
-                while (index != -1) {
-                    count++;
-                    input = input.substring(index + 1);
-                    index = input.indexOf(ActionConstants.ACTION_BACK);
-                }
-                if (count <= 1) {
-                    return false;
-                } else {
-                    return true;
-                }
-            }
+    public static boolean containsAction(ActionConfig config, String action) {
+        if (!actionConfigContainsAction(config, action)) {
+            return true;
+        } else {
+            return false;
         }
-        return false;
     }
 }
